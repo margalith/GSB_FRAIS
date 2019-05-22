98d71249b177b4161effdfbc08c8d9ae66953b9d
margalith <margalith@192.168.1.12>
2019-05-22 14:13:50 +0200

    doc ppe 2

--- controleurs/c_accueil.php
+++ controleurs/c_accueil.php
@@ -4,6 +4,8 @@
 $estVisiteurConnecte = estVisiteurConnecte();
 $estComptableConnecte = estComptableConnecte();
 
+
+
 if ($estVisiteurConnecte) {
 include
     'vues/v_accueilVisiteur.php';
@@ -11,6 +13,7 @@ include
   elseif 
     ($estComptableConnecte){
      include 'vues/v_accueilComptable.php';
+     
 } else {
     include 'vues/v_connexion.php';
 }
--- controleurs/c_validerFrais.php
+++ controleurs/c_validerFrais.php
@@ -4,25 +4,42 @@ $moisPrecedent= getMoisPrecedent($mois);
 $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
 $fichesCL = $pdo->ficheDuMoisCloturees($moisPrecedent);
 $pdo = PdoGsb::getPdoGsb();
-$lesVisiteurs = $pdo->getLesVisiteurs($pdo);
-$lesMois = getLesDouzeDerniersMois($mois);
+
 switch ($action) {
-case 'valider':
-    if ($fichesCL){
-        include 'vues/v_listesVisiteursMois.php';
-    }else{
-     $pdo->clotureFicheFrais($moisPrecedent); //cr en cl
+    case 'valider':
+        $lesVisiteurs = $pdo->getLesVisiteurs($pdo);
+        $lesMois = getLesDouzeDerniersMois($mois);
+        $lesClesM = array_keys($lesMois);
+        $moisASelectionner = $lesClesM[0];
+        $lesClesV = array_keys($lesVisiteurs);
+        $visiteurASelectionner = $lesClesV[24];
+        if ($fichesCL){
+            include 'vues/v_listesVisiteursMois.php';
+        }else{
+           $pdo->clotureFicheFrais($moisPrecedent); //cr en cl
+           include 'vues/v_listesVisiteursMois.php';
+       }
+    break;
+    case 'valideVM':
+       $leMois = filter_input(INPUT_POST, 'lstMois', FILTER_SANITIZE_STRING);
+       $leVisiteur = filter_input(INPUT_POST, 'lstVisiteurs', FILTER_SANITIZE_STRING);
+       $moisASelectionner = $leMois;   
+       $visiteurASelectionner = $leVisiteur; 
+       $lesMois = getLesDouzeDerniersMois($mois);
+       $lesVisiteurs = $pdo->getLesVisiteurs($pdo);
        include 'vues/v_listesVisiteursMois.php';
-   }
-   break;
-case 'ValideVM':
-   $leMois = filter_input(INPUT_POST, 'lstMois', FILTER_SANITIZE_STRING);
-   $leVisiteur = filter_input(INPUT_POST, 'lstVisiteurs', FILTER_SANITIZE_STRING);
-   $moisASelectionner = $leMois;   
-   $visiteurASelectionner = $leVisiteur; 
-   include 'vues/v_listesVisiteursMois.php';
-   break;
-default;
+       $lesFraisHorsForfait = $pdo->getLesFraisHorsForfait($leVisiteur, $leMois);
+       $lesFraisForfait = $pdo->getLesFraisForfait($leVisiteur, $leMois);
+       $infosFiche = $pdo ->getLesInfosFicheFrais($leVisiteur, $leMois);
+
+       if (!is_array($infosFiche)){
+           ajouterErreur("Pas de fiche frais pour ce visiteur ce mois");
+           include 'vues/v_erreurs.php';
+       }else{
+           include 'vues/v_afficheFrais.php';
+       }
+       break;
+    default;
 }
 
 
